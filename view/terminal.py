774e65f4ddcacc5130aecd092010e38cee447ff2
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2021-10-19 16:24:22 +0200

    print_table(table) needs refactoring

--- view/terminal.py
+++ view/terminal.py
@@ -1,31 +1,15 @@
 def print_menu(title, list_options):
-    """Prints options in standard menu format like this:
-
-
-    Main menu:
-    (1) Store manager
-    (2) Human resources manager
-    (3) Inventory manager
-    (0) Exit program
-
-    Args:
-        title (str): the title of the menu (first row)
-        list_options (list): list of the menu options (listed starting from 1, 0th element goes to the end)
-    """
     print(title)
+
     for i, option in enumerate(list_options):
         if i > 0:
             print(f'({i}) {option}')
+
     print(f'(0) {list_options[0]}')
 
 
 def print_message(message):
-    """Prints a single message to the terminal.
-
-    Args:
-        message: str - the message
-    """
-    print(message)
+    print(f"{message}\n")
 
 
 def print_general_results(result, label):
@@ -44,30 +28,80 @@ def print_general_results(result, label):
 # |--------|------------|----------|
 # |   1    | Sidewinder | missile  |
 # \-----------------------------------/
+# table = [[id,product,type],[0,Bazooka,portable]]
 def print_table(table):
-    """Prints tabular data like above.
+    number_of_rows = len(table)
+    number_of_cols = len(table[0])
 
-    Args:
-        table: list of lists - the table to print out
-    """
-    pass
+    columns_width = []
+    '''
+    for column_number in range(number_of_cols):
+        max_column_width = 0
+
+        for row_number in range(number_of_rows):
+            max_column_width = max(max_column_width,len(table[row_number][column_number]))
+            columns_width.append(max_column_width)
+    
+    table_width = sum(columns_width)
+    print(columns_width)
+    '''
+    for number_of_col in range(number_of_cols):
+        temp_list = []
+        for number_of_row in range(number_of_rows):
+            column_width = len(table[number_of_row][number_of_col]) 
+            column_width += 2
+            temp_list.append(column_width)
+        
+        max_column_width = max(temp_list)
+        
+        columns_width.append(max_column_width)
+
+    table_width = sum(columns_width)
+
+    print("/" + "-"*(table_width+(number_of_cols-1)) + "\\")
+
+    table_prepared_to_print = []
+    for i in range(len(table)):
+        
+        row_for_print = []
+
+        for j in range(len(table[i])):
+            column = table[i][j]
+            column_width = columns_width[j]
+            centered_data = column.center(column_width)
+            row_for_print.append("|")
+            row_for_print.append(centered_data)
+        
+        row_for_print.append("|")
+
+        delimiter_row = []
+
+        for j in range(len(table[i])):
+            column_width = columns_width[j]
+            centered_data = column.center(column_width)
+            delimiter_row.append("|")
+            delimiter_row.append("-"*column_width)
+        
+        delimiter_row.append("|")
+
+        table_prepared_to_print.append(row_for_print)
+        table_prepared_to_print.append(delimiter_row)
+
+    for i in range(len(table_prepared_to_print)-1):
+        row = table_prepared_to_print[i]
+        string_for_print = "".join(row)
+        print(string_for_print)
+
+    print("\\" + "-"*(table_width+(number_of_cols-1)) + "/" + "\n")
+
+print(print_table(table = [["id","product","type"],["0","Bazooka","portable"],["22","3333","456"]]))
 
 
 def get_input(label):
-    """Gets single string input from the user.
-
-    Args:
-        label: str - the label before the user prompt
-    """
-    return input(label)
+    return input(f"{label}:\t")
 
 
 def get_inputs(labels):
-    """Gets a list of string inputs from the user.
-
-    Args:
-        labels: list - the list of the labels to be displayed before each prompt
-    """
     labels_list = []
 
     for label in labels:
@@ -77,9 +111,4 @@ def get_inputs(labels):
 
 
 def print_error_message(message):
-    """Prints an error message to the terminal.
-
-    Args:
-        message: str - the error message
-    """
-    print(message)
+    print(f"{message}\n")
