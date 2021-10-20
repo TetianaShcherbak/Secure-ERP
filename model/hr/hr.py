ec486b2b484b179a517b6463695223fc3f024795
Grzegorz Dąbrowski <g.dabrowski@onekk.eu>
2021-10-20 21:40:59 +0200

    Add hr model

--- model/hr/hr.py
+++ model/hr/hr.py
@@ -8,7 +8,38 @@ Data table structure:
     - clearance level (int): from 0 (lowest) to 7 (highest)
 """
 
-from model import data_manager, util
+from model import util
+from model import crud
 
 DATAFILE = "model/hr/hr.csv"
 HEADERS = ["Id", "Name", "Date of birth", "Department", "Clearance"]
+
+
+def read():
+    return crud.crud_read(DATAFILE)
+
+
+def add(data_to_add):
+    user_id = util.generate_id()
+    data_to_add.insert(0,user_id)
+    return crud.crud_create(DATAFILE, data_to_add, user_id)
+
+
+def update(new_data_table):
+    return crud.crud_update(DATAFILE, new_data_table)
+
+
+def remove(user_id):
+    return crud.crud_delete(user_id, DATAFILE)
+
+
+def is_contained(user_id):
+    database = read()
+    column_for_check = []
+    for data in database:
+        column_for_check.append(data[0])
+    return user_id in column_for_check
+
+
+def get_oldest_and_youngest():
+    pass
