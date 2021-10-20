12bc3bb0d71340c178f86e683c6d619cf6a3c1ba
Grzegorz Dąbrowski <g.dabrowski@onekk.eu>
2021-10-20 21:42:14 +0200

    Add sales controller (in progress)

--- model/sales/sales.py
+++ model/sales/sales.py
@@ -7,8 +7,28 @@ Data table structure:
     - price (float)
     - transaction date (string): in ISO 8601 format (like 1989-03-21)
 """
-
-from model import data_manager, util
+from model import data_manager, util, crud
 
 DATAFILE = "model/sales/sales.csv"
 HEADERS = ["Id", "Customer", "Product", "Price", "Date"]
+
+def read():
+    return crud.crud_read(DATAFILE)
+
+def add(data_to_add):
+    trade_id = util.generate_id()
+    data_to_add.insert(0,trade_id)
+    return crud.crud_create(DATAFILE, data_to_add, trade_id)
+
+def update(new_data_table):
+    return crud.crud_update(DATAFILE, new_data_table)
+
+def is_contained(user_id):
+    database = read()
+    column_for_check = []
+    for data in database:
+        column_for_check.append(data[0])
+    return user_id in column_for_check
+
+def remove(user_id):
+   return crud.crud_delete(DATAFILE,user_id)
