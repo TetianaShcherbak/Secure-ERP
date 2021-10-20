af54f8178ff7a4022ad95e0f48b36c678432b1cc
Grzegorz Dąbrowski <g.dabrowski@onekk.eu>
2021-10-20 21:39:25 +0200

    Add util with id generator

--- model/util.py
+++ model/util.py
@@ -6,5 +6,26 @@ def generate_id(number_of_small_letters=4,
                 number_of_capital_letters=2,
                 number_of_digits=2,
                 number_of_special_chars=2,
-                allowed_special_chars=r"_+-!"):
-    return 'T!uq6-b4Yq'
+                allowed_specialchars=r"+-!"):
+
+    small_letters = list(string.ascii_letters.lower())
+    capital_letters = list(string.ascii_letters.upper())
+    digits = list(string.digits)
+    special_characters = list(allowed_specialchars)
+    id_number = []
+
+    for i in range(number_of_small_letters):
+        id_number.append(random.choice(small_letters))
+
+    for i in range(number_of_capital_letters):
+        id_number.append(random.choice(capital_letters))
+
+    for i in range(number_of_digits):
+        id_number.append(random.choice(digits))
+
+    for i in range(number_of_special_chars):
+        id_number.append(random.choice(special_characters))
+
+    random.shuffle(id_number)
+
+    return "".join(id_number)
