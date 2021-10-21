e7bc9dc2bbf90f752169d629de4291649f496b48
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2021-10-21 09:34:35 +0200

    merge in progress

--- controller/crm_controller.py
+++ controller/crm_controller.py
@@ -3,23 +3,49 @@ from view import terminal as view
 
 
 def list_customers():
-    view.print_error_message("Not implemented yet.")
+    data = crm.get_data()
+
+    view.print_table(data, crm.HEADERS)
 
 
 def add_customer():
-    view.print_error_message("Not implemented yet.")
+    customer_data = view.get_inputs(["Enter customer name: ", "Enter customer email: ", "Enter customer subscription status (1 or 0): "])
+    
+    if crm.add(customer_data):
+        view.print_message(f"User's data has been created")
+    else:
+        view.print_error_message(f"User's data has not been created")
 
 
 def update_customer():
-    view.print_error_message("Not implemented yet.")
+    customer_id = view.get_input('Please enter the customer ID')
+    
+    if crm.check_customer(customer_id):
+        customer_data = view.get_inputs(["Enter customer name: ", "Enter customer email: ", "Enter customer subscription status (1 or 0): "])
+        
+        customer_data.insert(0,customer_id)
+        crm.update(customer_data)
+
+        view.print_message('Database has been updated')
+    else:
+        view.print_message('Customer doesn\'t exist')
 
 
 def delete_customer():
-    view.print_error_message("Not implemented yet.")
+    customer_id = view.get_input("Please provide valid user id")
+
+    if crm.remove(customer_id):
+        view.print_error_message(f'User id {customer_id} removed.')
+    else:
+        view.print_error_message(f'User id {customer_id} not found, no records removed.')
 
 
 def get_subscribed_emails():
-    view.print_error_message("Not implemented yet.")
+    data = crm.select_subscribed_emails()
+
+    headers = ['Subscribed emails']
+    
+    view.print_table(data, headers)
 
 
 def run_operation(option):
--- model/crm/crm.py
+++ model/crm/crm.py
@@ -7,8 +7,44 @@ Data table structure:
     - subscribed (int): Is subscribed to the newsletter? 1: yes, 0: no
 """
 
-from model import data_manager, util
+from model import util, crud
 
 
 DATAFILE = "model/crm/crm.csv"
 HEADERS = ["id", "name", "email", "subscribed"]
+
+
+def remove(user_id):
+    return crud.crud_delete(DATAFILE,user_id)
+
+
+def add(data_to_add):
+    user_id = util.generate_id()
+    data_to_add.insert(0,user_id)
+    return crud.crud_create(DATAFILE, data_to_add, user_id)
+
+
+def update(new_data_table):
+    
+    return crud.crud_update(DATAFILE, new_data_table)
+
+
+def get_data():
+    return crud.crud_read(DATAFILE)
+
+
+def select_subscribed_emails():
+    data = crud.crud_read(DATAFILE)
+    subscribed_emails = []
+    for entry in data:
+        if entry[3] == '1':
+            subscribed_emails.append([entry[2]])
+    return subscribed_emails
+
+
+def check_customer(customer_id):
+    database = crud.crud_read(DATAFILE)
+    for entry in database:
+        if entry[0] == customer_id:
+            return True
+    return False
